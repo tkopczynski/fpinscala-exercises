8281d9a388e2b7cb8a41e7f94b0e35d5ab47f3f1
Tomek Kopczynski <tomek@kopczynski.net.pl>
2017-08-18 22:39:00 +0200

    chapter 2

--- src/main/scala/Chapter2.scala
+++ src/main/scala/Chapter2.scala
@@ -16,6 +16,7 @@ object Chapter2 {
   }
 
   def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
     def go(n: Int): Boolean = {
       if (n == as.length) true
       else if (! ordered(as(n - 1), as(n))) false
@@ -23,6 +24,21 @@ object Chapter2 {
     }
 
     go(1)
+  }
+
+  // 2.3
+  def curry[A,B,C](f: (A,B) => C): A => (B => C) = {
+    a => b => f(a, b)
+  }
+
+  // 2.4
+  def uncurry[A,B,C](f: A => B => C): (A,B) => C = {
+    (a,b) => f(a)(b)
+  }
+
+  // 2.5
+  def compose[A,B,C](f: B => C, g: A => B): A => C = {
+    a => f(g(a))
   }
 
   def main(args: Array[String]): Unit = {
